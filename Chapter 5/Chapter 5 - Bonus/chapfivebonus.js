2a5336579b52d0fb9264ac70e4cc47c2b5525c2c
Bay <144781991+BaylaDeLemos@users.noreply.github.com>
2024-03-21 11:49:36 +0400

    chapfivebonus.js

--- Chapter 5/Chapter 5 - Bonus/chapfivebonus.js
+++ Chapter 5/Chapter 5 - Bonus/chapfivebonus.js
@@ -1,40 +1,53 @@
+// Function to play audio for button 0
 function play() {
-  var audio = document.getElementById("audio");
-  audio.play();
+  var audio = document.getElementById("audio"); // Get audio element by ID
+  audio.play(); // Play the audio
 }
 
+// Function to play audio for button 1
 function play1() {
-  var audio = document.getElementById("audio1");
-  audio.play();
+  var audio = document.getElementById("audio1"); // Get audio element by ID
+  audio.play(); // Play the audio
 }
 
+// Function to play audio for button 2
 function play2() {
-  var audio = document.getElementById("audio2");
-  audio.play();
-}
-function play3() {
-  var audio = document.getElementById("audio3");
-  audio.play();
-}
-function play4() {
-  var audio = document.getElementById("audio4");
-  audio.play();
-}
-function play5() {
-  var audio = document.getElementById("audio5");
-  audio.play();
-}
-function play6() {
-  var audio = document.getElementById("audio6");
-  audio.play();
-}
-function play7() {
-  var audio = document.getElementById("audio7");
-  audio.play();
-}
-function play8() {
-  var audio = document.getElementById("audio8");
-  audio.play();
+  var audio = document.getElementById("audio2"); // Get audio element by ID
+  audio.play(); // Play the audio
 }
 
-/*ezctrlc+ctrlv*/
+// Function to play audio for button 3
+function play3() {
+  var audio = document.getElementById("audio3"); // Get audio element by ID
+  audio.play(); // Play the audio
+}
+
+// Function to play audio for button 4
+function play4() {
+  var audio = document.getElementById("audio4"); // Get audio element by ID
+  audio.play(); // Play the audio
+}
+
+// Function to play audio for button 5
+function play5() {
+  var audio = document.getElementById("audio5"); // Get audio element by ID
+  audio.play(); // Play the audio
+}
+
+// Function to play audio for button 6
+function play6() {
+  var audio = document.getElementById("audio6"); // Get audio element by ID
+  audio.play(); // Play the audio
+}
+
+// Function to play audio for button 7
+function play7() {
+  var audio = document.getElementById("audio7"); // Get audio element by ID
+  audio.play(); // Play the audio
+}
+
+// Function to play audio for button 8
+function play8() {
+  var audio = document.getElementById("audio8"); // Get audio element by ID
+  audio.play(); // Play the audio
+}
